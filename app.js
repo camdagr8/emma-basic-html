77da4c08d734efaf9560ee3b47be304f839ddf13
Cam Tullos <camtullos@us.ibm.com>
2018-06-27 10:37:21 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 
 
-function clickme (e) {
-    alert('Hey guys!');
+window.addEventListener('load', initialize);
+
+
+function progressUpdate() {
+    var pbars = document.getElementsByClassName('progress-bar');
+    var pvals = document.getElementsByClassName('progress-value');
+
+    for (var i = 0; i < pbars.length; i++) {
+        var progress = pbars[i].dataset.progress;
+        if (!progress) { break; }
+        pbars[i].style.width = progress + '%';
+        pvals[i].innerHTML = progress + '%';
+    }
+}
+
+function initialize() {
+    progressUpdate();
 }
